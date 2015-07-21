6bc9b15315ae89a1343192b556b205e5adbe6dab
Matthijs Dekker <github@plamola.com>
2015-07-21 15:35:39 +0200

    Update vamp.rb

--- vamp.rb
+++ vamp.rb
@@ -2,12 +2,14 @@ require "formula"
 
 class Vamp < Formula
   homepage "http://vamp.io"
-  url "https://bintray.com/artifact/download/magnetic-io/downloads/vamp-cli/vamp-cli-0.7.8-brew.zip"
+  version "0.7.8-brew"
+  url "https://bintray.com/artifact/download/magnetic-io/downloads/vamp-cli/vamp-cli-#{version}.zip"
+  # generate the sha256 hash on your mac with the command: shasum -a 256 <filename>
   sha256 "da48f0416daba86cb77a051866c9960f1902a7707045981b5bc76591f449e7d1"
-
+  
   def install
-      inreplace "vamp-cli-0.7.8-brew/brew/vamp", "##PREFIX##", "#{prefix}"
-      prefix.install "vamp-cli-0.7.8-brew/lib/vamp-cli.jar"
-      bin.install "vamp-cli-0.7.8-brew/brew/vamp"
+      inreplace "vamp-cli-#{version}/brew/vamp", "##PREFIX##", "#{prefix}"
+      prefix.install "vamp-#{version}/lib/vamp-cli.jar"
+      bin.install "vamp-cli-#{version}/brew/vamp"
   end
 end
