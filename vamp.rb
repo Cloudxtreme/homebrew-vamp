d9f5b59e234127580f722ffad612c085c92b39ee
Matthijs Dekker <github@plamola.com>
2015-07-21 15:04:44 +0200

    Update vamp.rb

--- vamp.rb
+++ vamp.rb
@@ -1,6 +1,6 @@
 require "formula"
 
-class VampUI < Formula
+class Vamp < Formula
   homepage "http://vamp.io"
   url "https://bintray.com/artifact/download/magnetic-io/downloads/vamp-cli/vamp-cli-0.7.8-brew.zip"
   sha256 "cfd1620cb48247e891292d48424a20c5f59bac6a6651ddafba74b7dbe6cedd2f"
