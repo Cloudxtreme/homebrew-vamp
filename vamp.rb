555127ce4e971281e5c8060c32b7c30bc346f9c2
Matthijs Dekker <github@plamola.com>
2015-07-21 15:27:45 +0200

    Update vamp.rb

--- vamp.rb
+++ vamp.rb
@@ -6,8 +6,8 @@ class Vamp < Formula
   sha256 "da48f0416daba86cb77a051866c9960f1902a7707045981b5bc76591f449e7d1"
 
   def install
-      inreplace "vamp-cli-0.7.8.2/brew/vamp", "##PREFIX##", "#{prefix}"
-      prefix.install "vamp-cli-0.7.8.2/lib/vamp-cli.jar"
-      bin.install "vamp-cli-0.7.8.2/brew/vamp"
+      inreplace "vamp-cli-0.7.8-brew/brew/vamp", "##PREFIX##", "#{prefix}"
+      prefix.install "vamp-cli-0.7.8-brew/lib/vamp-cli.jar"
+      bin.install "vamp-cli-0.7.8-brew/brew/vamp"
   end
 end
