e282524291c0c5063e2273eed44d0fbd31adcb7b
Matthijs Dekker <github@plamola.com>
2015-07-21 15:23:59 +0200

    Update vamp.rb

--- vamp.rb
+++ vamp.rb
@@ -3,11 +3,11 @@ require "formula"
 class Vamp < Formula
   homepage "http://vamp.io"
   url "https://bintray.com/artifact/download/magnetic-io/downloads/vamp-cli/vamp-cli-0.7.8-brew.zip"
-  sha256 "cfd1620cb48247e891292d48424a20c5f59bac6a6651ddafba74b7dbe6cedd2f"
+  sha256 "da48f0416daba86cb77a051866c9960f1902a7707045981b5bc76591f449e7d1"
 
   def install
       inreplace "vamp-cli-0.7.8.2/brew/vamp", "##PREFIX##", "#{prefix}"
-      prefix.install "lib/vamp-cli.jar"
+      prefix.install "vamp-cli-0.7.8.2/lib/vamp-cli.jar"
       bin.install "vamp-cli-0.7.8.2/brew/vamp"
   end
 end
