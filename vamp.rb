57163964a2d380c3e6e8e3654a1e9985a321e82b
Matthijs Dekker <github@plamola.com>
2015-07-21 16:11:42 +0200

    Update vamp.rb

--- vamp.rb
+++ vamp.rb
@@ -2,7 +2,7 @@ require "formula"
 
 class Vamp < Formula
   homepage "http://vamp.io"
-  version "0.7.8-brew"
+  version "0.7.8.3"
   url "https://bintray.com/artifact/download/magnetic-io/downloads/vamp-cli/vamp-cli-#{version}.zip"
   # generate the sha256 hash on your mac with the command: shasum -a 256 <filename>
   sha256 "da48f0416daba86cb77a051866c9960f1902a7707045981b5bc76591f449e7d1"
