0838626b422dc00eb4bbac53657f4548eb51ebee
Jeroen Gordijn <jeroen.gordijn@gmail.com>
2015-10-23 15:13:37 +0200

    Update vamp.rb
    Update brew formula to version 0.7.11

--- vamp.rb
+++ vamp.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class Vamp < Formula
   homepage "http://vamp.io"
-  version "0.7.10"
+  version "0.7.11"
   url "https://bintray.com/artifact/download/magnetic-io/downloads/vamp-cli/vamp-cli-#{version}.zip"
   # generate the sha256 hash on your mac with the command: shasum -a 256 <filename>
-  sha256 "52319025ecb9bfc51759cefdec847487686c0f4ae5dce2f7a001e6b65d6ff548"
+  sha256 "64ce24cbd65ceddd2917c23a702cbe45a3037ac77db7428494b3ee4db1ca9bdc"
   
   def install
       inreplace "brew/vamp", "##PREFIX##", "#{prefix}"
