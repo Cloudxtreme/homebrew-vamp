91308d92aeadecdff986aac79b316be5ebc8f437
dragoslav <dragoslav@users.noreply.github.com>
2016-01-04 12:16:08 +0100

    version 0.8.1

--- vamp.rb
+++ vamp.rb
@@ -2,10 +2,10 @@ require "formula"
 
 class Vamp < Formula
   homepage "http://vamp.io"
-  version "0.8.0"
+  version "0.8.1"
   url "https://bintray.com/artifact/download/magnetic-io/downloads/vamp-cli/vamp-cli-#{version}.zip"
   # generate the sha256 hash on your mac with the command: shasum -a 256 <filename>
-  sha256 "6d4c7c0dda6809bf15d50b2e81ba5eea8b5c53e6d7e69f44fac9e519566b9a7f"
+  sha256 "c11e8196cff9b59ea341d9ee85c08ce5e6d364a86e6c408c4192844259b6ea56"
   
   def install
       inreplace "brew/vamp", "##PREFIX##", "#{prefix}"
